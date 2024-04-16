900d4d4a5a59672d2c94c91bdf3bddb6264fea87
Stephanie Lenehan <stephanie.lenehan@gmail.com>
2024-04-16 10:48:25 +0000

    add SCOPE for IAM config

--- run.py
+++ run.py
@@ -1,4 +1,12 @@
 import gspread
 from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+    
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
