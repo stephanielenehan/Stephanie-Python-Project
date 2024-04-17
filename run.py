9534b1af0a93a6bb578288c5ada785693e408a75
Stephanie Lenehan <stephanie.lenehan@gmail.com>
2024-04-17 17:57:29 +0000

    get example function to run in terminal

--- run.py
+++ run.py
@@ -15,6 +15,21 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('stephanie-python-project')
 
-sales = SHEET.worksheet('sales')
-data = sales.get_all_values()
-print(data)
+#sales = SHEET.worksheet('sales')
+#data = sales.get_all_values()
+#print(data)
+
+
+# example function
+def get_sales_data():
+    """
+    Get sales figures from the user
+    """
+    print("Please enter sales data from the last market.")
+    print("Data should be six numbers, separated by commas.")
+    print("Example: 10,20,30,40,50,60\n")
+
+    data_str = input("Enter your data here: ")
+    print(f"The data provided is: {data_str}")
+
+get_sales_data()
