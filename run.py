c5e2997afc478133c8c529f1d6140b7c7938631c
Stephanie Lenehan <stephanie.lenehan@gmail.com>
2024-04-18 12:07:01 +0000

    start of opening_page function - testing tbc

--- run.py
+++ run.py
@@ -32,4 +32,59 @@ def get_sales_data():
     data_str = input("Enter your data here: ")
     print(f"The data provided is: {data_str}")
 
-get_sales_data()
+def opening_page():
+    """
+    Function to display welcome to the user, 
+    describe the program & time required to the user and
+    ask the user if they are ready to proceed? 
+    or exit if they do not wish to proceed
+    """
+    while True:
+        print ("\nWelcome To Your Daily Meal & Ingredients Planner.\n")
+        print ("This is a short Python program that will show you a shopping")
+        print("list of ingredients based on your meal choices for tomorrow.\n")
+        print ("It will take approx. 5 mins\n")
+        print ("Are you ready to do this now? Y = Yes  N = No\n")
+        user_decision = input ("Enter: \n")
+        user_decision = user_decision.strip() # remove whitespace before &/or after user input
+        if user_decision == "Y" or user_decision == "y": # allow uppercase or lowercase Y/y
+            print("------------------------------") # 30 * - character
+            print("\nGreat, Let's Go!\n")
+            break
+        elif user_decision == "N" or user_decision == "n": # allow uppercase or lowercase N/n
+            ("------------------------------") # 30 * - character
+            print("\nOK, See You Later. Bye!")
+            break
+        else:
+            print ("\nPlease Try Again")
+            print ("\nOnly Enter Y or N")
+            print("------------------------------") # 30 * - character
+            return opening_page()         
+
+
+def main():
+    """
+    Run all program functions
+    """
+    opening_page()
+
+
+main()
+
+
+
+
+
+
+
+
+
+
+
+# Testing opening_page() function
+# Capital Y 
+# Lowercase y 
+# Capital N 
+# Lowercase n  
+# White Space before &/or after 
+# Invalid input 
